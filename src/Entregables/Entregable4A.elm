d69fc03ebd38b68ab97156e03d287b8b4ad03771
mikeldalmauc <mikeldalmauc@gmail.com>
2025-04-03 16:05:12 +0200

    video

--- src/Entregables/Entregable4A.elm
+++ src/Entregables/Entregable4A.elm
@@ -28,4 +28,30 @@ view d =
 
 content : Dimensions -> Element msg
 content d =
-    wip d
+    column []
+        [ MarkdownThemed.renderFull
+            """
+
+# 4.1 Evaluación previa para diferenciar los niveles de conocimiento
+
+- [Tema](#tema)
+- [Evaluación previa](#evaluación-previa)
+
+
+## Tema
+Como tema he elegido la creación de animaciones en Android usando Kotlin. Se trata de un tema interesante y relevante en el desarrollo de interfaces, ayuda a desarrollar la creatividad y la resolucion de problemas. El prompt para un reto podría ser:
+
+- *Una empresa de desarrollo de aplicaciones de navegación, ha solicitado crear unos controles de navegación animados para su aplicación. El objetivo es crear un joystick que mejore la experiencia del usuario y haga la navegación más intuitiva. Se espera que las animaciones sean fluidas y representen el estado del joystick fielmente.*
+
+## Evaluación previa 
+
+La evaluación se compone por un conjunto de preguntas que determinarán el nivel de conocimiento de los alumnos sobre las herramientas y conocimientos que requiere desarrollar un joystick.
+
+[Evaluación previa](https://docs.google.com/forms/d/e/1FAIpQLScq1jq_qpnpoM43eEsRHCM0_vL1YwFitKAdKj3A-GJHYNhwfA/viewform?usp=dialog)
+
+## Vídeo
+
+
+"""
+        , videoView d "https://www.youtube.com/embed/bLqDzeTvMA4?si=a28mboA_B6l5vBeG"
+        ]
